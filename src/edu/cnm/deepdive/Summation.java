7a33cf3ec5eedfee32f74ddffa32ac5b19b3a5df
Michael Lacey <laceymwes@gmail.com>
2018-05-21 16:28:35 -0600

    Add while loop.

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -1,6 +1,9 @@
 package edu.cnm.deepdive;
+
 /**
- * This is the class containing the main entry point for the (eventual) RPN calculator.
+ * This is the class containing the main entry point for the (eventual) RPN
+ * calculator.
+ * 
  * @author Michael Lacey
  * @version 0.1
  * 
@@ -10,16 +13,26 @@ public class Summation {
 	/**
 	 * Command-line entry point for RPN interpreter
 	 * 
-	 * @param args  operands and operators.
+	 * @param args
+	 *            operands and operators.
 	 */
 	public static void main(String[] args) {
 		int sum = 0;
-		
-		for (int i = 0; i < args.length; ++i) {
+
+		// for (int i = 0; i < args.length; ++i) {
+		// int val = Integer.parseInt(args[i]);
+		// sum += val;
+		// }
+
+		int i = 0;
+		while (i < args.length) {
+			args[i] = args[i].trim();
 			int val = Integer.parseInt(args[i]);
 			sum += val;
+			i++;
 		}
+
 		System.out.println(sum);
-		
+
 	}
 }
