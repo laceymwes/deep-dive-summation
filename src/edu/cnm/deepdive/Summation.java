f49b09cbb802c22f39170c563ae732d6e114efa4
Michael Lacey <laceymwes@gmail.com>
2018-05-22 11:56:58 -0600

    Add multiplication, division, and exponentiation.

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -2,9 +2,8 @@ package edu.cnm.deepdive;
 
 import java.util.Stack;
 
-
 /**
- * Command-line entry point for RPN interpreter. 
+ * Command-line entry point for RPN interpreter.
  * 
  * @author Michael Lacey
  * @version 0.1
@@ -21,25 +20,56 @@ public class Summation {
 	public static void main(String[] args) {
 		Stack<Double> stack = new Stack<>();
 		for (int i = 0; i < args.length; i++) {
-			args[i] = args[i].trim();
-			if(args[i].equals("+")){
-				double val1 = stack.pop();
-				double val2 = stack.pop();
+			args[i] = args[i].trim().toLowerCase();
+			String arg = args[i];
+			/*
+			 * if(arg.equals("+")){ double val1 = stack.pop(); double val2 = stack.pop();
+			 * stack.push(val1 + val2); } else if(arg.equals("-")) { double val1 =
+			 * stack.pop(); double val2 = stack.pop(); stack.push(val1 - val2); } else
+			 * if(arg.equals("*")) { double val1 = stack.pop(); double val2 = stack.pop();
+			 * stack.push(val1 * val2); } else { double val = Double.parseDouble(arg);
+			 * stack.push(val); }
+			 */
+			double val1;
+			double val2;
+			switch (arg) {
+			// FIXME: Replace repeated val1 and val2 assignment statements with method call.
+			case "add":
+			case "+":
+				val1 = stack.pop();
+				val2 = stack.pop();
 				stack.push(val1 + val2);
-			}
-			else if(args[i].equals("-")) {
-				double val1 = stack.pop();
-				double val2 = stack.pop();
+				break;
+			case "sub":
+			case "-":
+				val1 = stack.pop();
+				val2 = stack.pop();
 				stack.push(val1 - val2);
+				break;
+			case "mul":
+			case "*":
+				val1 = stack.pop();
+				val2 = stack.pop();
+				stack.push(val1 * val2);
+				break;
+			case "div":
+			case "/":
+				val1 = stack.pop();
+				val2 = stack.pop();
+				stack.push(val1 / val2);
+				break;
+			case "^":
+			case "exp":
+				val1 = stack.pop();
+				val2 = stack.pop();
+				stack.push(Math.pow(val1, val2));
+				break;
+			default:
+				val1 = Double.parseDouble(arg);
+				stack.push(val1);
+				break;
 			}
-			else {
-				double val = Double.parseDouble(args[i]);
-				stack.push(val); 
-			}
-			
-			
 		}
-		System.out.printf("Result = %,.2f", stack.pop()); 
-
+		System.out.printf("Result = %,.2f", stack.pop());
 	}
 }
